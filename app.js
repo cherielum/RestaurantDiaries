5a9298132f5e20d0ec638e686f447c564cd9b999
Cherie Lum <charismatic.cherie@gmail.com>
2017-11-14 20:54:05 -0500

    adding model

---
+++ app.js
@@ -0,0 +1,60 @@
+
+//=================
+//RESTAURANT MODEL 
+//=================
+
+const Sequelize = require('sequelize');
+const restaurant = new Sequelize('postgres://Cherie:@localhost:5432/restaurant');
+
+restaurant.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Restaurant = restaurant.define('restaurant', {
+    name: {
+      type: Sequelize.STRING
+    },
+    rating: {
+      type: Sequelize.INTEGER
+    },
+    review: {
+      type: Sequelize.STRING
+    },
+    wouldYouGoBack: {
+      type: Sequelize.BOOLEAN
+    },
+    category: {
+      type: Sequelize.STRING
+    },
+     type: {
+      type: Sequelize.STRING
+    },
+     lastVisted: {
+      type: Sequelize.DATE
+    },
+     cleanBathroom: {
+      type: Sequelize.BOOLEAN
+    }
+  });
+
+  Restaurant.sync().then(() => {
+  // Table created
+  return Restaurant.create({
+    name: 'Chipotle',
+    rating: '3',
+    review: 'The rice bowl rocks!!! ',
+    wouldYouGoBack: true,
+    category: 'Fast Casual',
+    type: 'Mexican',
+    lastVisted: '2017-11-13',
+    cleanBathroom: true
+  });
+});
+
+
+Restaurant.findAll().then(restaurants => {
+  console.log(restaurants)
+});
